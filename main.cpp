ea38f3ede017f69bdeb50f64af72906246632058
Yawbee49 <oforiaduy@gmail.com>
2025-04-05 17:07:50 +0000

    Assignment 1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "**********************************" << endl;
+    cout << "* Programming Assignment 1       *" << endl;
+    cout << "* Computer Programming I         *" << endl;
+    cout << "* Author: Yaw Barimah Ofori - Adu*" << endl;
+    cout << "* Due Date: Thursday, Jan. 24    *" << endl;
+    cout << "**********************************" << endl;
+
+    return 0;
+}
